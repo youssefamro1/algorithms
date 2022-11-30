f7433c3bd0ea031b62d04d9f2ed0ac56d5581f22
youssef amr momtaz <77958872+youssefamro1@users.noreply.github.com>
2022-11-30 21:25:09 +0200

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,15 @@
+def insertionsort(A):
+    for j in range(1,len(A)):
+        key = A[j] 
+        i = j-1 
+        while (i > -1) and key < A[i]: 
+            A[i+1]=A[i] 
+            i=i-1 
+        A[i+1] = key
+    return A
+
+
+
+x = [5,2,4,6,1,3]
+insertionsort(x)
+print (x)
